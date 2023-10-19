3eb0c3f0d38271c81990aa8414a4a927e34c6280
Blhasn-Sehli <sehlibellhassen@gmail.com>
2023-10-19 21:53:17 +0200

    ok home Done

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -241,7 +241,7 @@ const Home = () => {
                 </h2>
                 <p className='lightParag' >Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
               </span>
-              products
+              {/* products */}
               <div className='productSection'>
                 <ProductCardContainer />
               </div>
